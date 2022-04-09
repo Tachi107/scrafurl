0aef52d24f57bef4480a4293934d59b7538f06c4
Andrea Pappacoda <andrea@pappacoda.it>
2022-04-09 11:48:19 +0200

    style: switch_to_snake_case
    Mostly, class names are still in JavaCase, just like in Rust.

--- include/scrafurl.hpp
+++ include/scrafurl.hpp
@@ -22,91 +22,91 @@ public:
 	~Scrafurl();
 
 	void get(const std::string_view url, const Text auto... headers) noexcept {
-		_responseBody.clear();
+		_response_body.clear();
 		curl_easy_setopt(curl, CURLOPT_HTTPGET, 1L);
 		curl_easy_setopt(curl, CURLOPT_URL, url.data());
-		struct curl_slist* curlHeaders {nullptr};
+		struct curl_slist* curl_headers {nullptr};
 		if constexpr (sizeof...(headers) != 0) {
-			const std::array headerArray {static_cast<std::string_view>(headers)...};
-			for (const std::string_view header : headerArray) {
-				curlHeaders = curl_slist_append(curlHeaders, header.data());
+			const std::array header_array {static_cast<std::string_view>(headers)...};
+			for (const std::string_view header : header_array) {
+				curl_headers = curl_slist_append(curl_headers, header.data());
 			}
 		}
-		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, curlHeaders);
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, curl_headers);
 		curl_easy_perform(curl);
-		curl_slist_free_all(curlHeaders);
+		curl_slist_free_all(curl_headers);
 	}
 
 	void post(const std::string_view url, const std::string_view request, const Text auto... headers) noexcept {
-		_responseBody.clear();
+		_response_body.clear();
 		curl_easy_setopt(curl, CURLOPT_POST, 1L);
 		curl_easy_setopt(curl, CURLOPT_URL, url.data());
-		struct curl_slist* curlHeaders {nullptr};
+		struct curl_slist* curl_headers {nullptr};
 		if constexpr (sizeof...(headers) != 0) {
-			const std::array headerArray {static_cast<std::string_view>(headers)...};
-			for (const std::string_view header : headerArray) {
-				curlHeaders = curl_slist_append(curlHeaders, header.data());
+			const std::array header_array {static_cast<std::string_view>(headers)...};
+			for (const std::string_view header : header_array) {
+				curl_headers = curl_slist_append(curl_headers, header.data());
 			}
 		}
-		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, curlHeaders);
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, curl_headers);
 		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, request.data());
 		curl_easy_perform(curl);
-		curl_slist_free_all(curlHeaders);
+		curl_slist_free_all(curl_headers);
 	}
 
 	void put(const std::string_view url, const std::string_view request, const Text auto... headers) noexcept {
-		_responseBody.clear();
+		_response_body.clear();
 		curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PUT");
 		curl_easy_setopt(curl, CURLOPT_URL, url.data());
-		struct curl_slist* curlHeaders {nullptr};
+		struct curl_slist* curl_headers {nullptr};
 		if constexpr (sizeof...(headers) != 0) {
-			const std::array headerArray {static_cast<std::string_view>(headers)...};
-			for (const std::string_view header : headerArray) {
-				curlHeaders = curl_slist_append(curlHeaders, header.data());
+			const std::array header_array {static_cast<std::string_view>(headers)...};
+			for (const std::string_view header : header_array) {
+				curl_headers = curl_slist_append(curl_headers, header.data());
 			}
 		}
-		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, curlHeaders);
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, curl_headers);
 		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, request.data());
 		curl_easy_perform(curl);
-		curl_slist_free_all(curlHeaders);
+		curl_slist_free_all(curl_headers);
 	}
 
 	void patch(const std::string_view url, const std::string_view request, const Text auto... headers) noexcept {
-		_responseBody.clear();
+		_response_body.clear();
 		curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PATCH");
 		curl_easy_setopt(curl, CURLOPT_URL, url.data());
-		struct curl_slist* curlHeaders {nullptr};
+		struct curl_slist* curl_headers {nullptr};
 		if constexpr (sizeof...(headers) != 0) {
-			const std::array headerArray {static_cast<std::string_view>(headers)...};
-			for (const std::string_view header : headerArray) {
-				curlHeaders = curl_slist_append(curlHeaders, header.data());
+			const std::array header_array {static_cast<std::string_view>(headers)...};
+			for (const std::string_view header : header_array) {
+				curl_headers = curl_slist_append(curl_headers, header.data());
 			}
 		}
-		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, curlHeaders);
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, curl_headers);
 		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, request.data());
 		curl_easy_perform(curl);
-		curl_slist_free_all(curlHeaders);
+		curl_slist_free_all(curl_headers);
 	}
 
 	void deletee(const std::string_view url, const Text auto... headers) noexcept {
-		_responseBody.clear();
+		_response_body.clear();
 		curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "DELETE");
 		curl_easy_setopt(curl, CURLOPT_URL, url.data());
-		struct curl_slist* curlHeaders {nullptr};
+		struct curl_slist* curl_headers {nullptr};
 		if constexpr (sizeof...(headers) != 0) {
-			const std::array headerArray {static_cast<std::string_view>(headers)...};
-			for (const std::string_view header : headerArray) {
-				curlHeaders = curl_slist_append(curlHeaders, header.data());
+			const std::array header_array {static_cast<std::string_view>(headers)...};
+			for (const std::string_view header : header_array) {
+				curl_headers = curl_slist_append(curl_headers, header.data());
 			}
 		}
-		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, curlHeaders);
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, curl_headers);
 		curl_easy_perform(curl);
-		curl_slist_free_all(curlHeaders);
+		curl_slist_free_all(curl_headers);
 	}
 
-	[[nodiscard]] long getResponseCode() const noexcept;
+	[[nodiscard]] long get_response_code() const noexcept;
 
-	[[nodiscard]] const std::string& getResponseBody() const noexcept;
+	[[nodiscard]] const std::string& get_response_body() const noexcept;
 
 	enum class HttpVersion : long {
 		v1_1 = CURL_HTTP_VERSION_1_1,
@@ -117,10 +117,10 @@ public:
 
 private:
 	CURL* curl;
-	std::string _responseBody;
-	long _responseCode;
-	static std::once_flag _isCurlInitialised;
+	std::string _response_body;
+	long _response_code;
+	static std::once_flag _is_curl_initialised;
 
 private:
-	static std::size_t writeData(char* incomingBuffer, std::size_t size, std::size_t count, std::string* data);
+	static std::size_t write_data(char* incomingBuffer, std::size_t size, std::size_t count, std::string* data);
 };
--- src/scrafurl.cpp
+++ src/scrafurl.cpp
@@ -8,39 +8,39 @@
 #include <cstdlib> /* std::atexit */
 
 // Must define static class members in cpp units
-std::once_flag Scrafurl::_isCurlInitialised;
+std::once_flag Scrafurl::_is_curl_initialised;
 
 Scrafurl::Scrafurl() {
-	std::call_once(_isCurlInitialised, []() noexcept {
+	std::call_once(_is_curl_initialised, []() noexcept {
 		curl_global_init(CURL_GLOBAL_NOTHING);
 		std::atexit(curl_global_cleanup);
 	});
 	curl = curl_easy_init();
 	curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
 	curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1L);
-	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeData);
-	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &_responseBody);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &_response_body);
 	curl_easy_setopt(curl, CURLOPT_TCP_FASTOPEN, 1L);
 	curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 1L);
 	curl_easy_setopt(curl, CURLOPT_PROTOCOLS, CURLPROTO_HTTP | CURLPROTO_HTTPS);
 	curl_easy_setopt(curl, CURLOPT_DEFAULT_PROTOCOL, "http");
-	_responseBody.reserve(CURL_MAX_WRITE_SIZE);
+	_response_body.reserve(CURL_MAX_WRITE_SIZE);
 }
 
 Scrafurl::~Scrafurl() {
 	curl_easy_cleanup(curl);
 }
 
-[[nodiscard]] long Scrafurl::getResponseCode() const noexcept {
-	curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &_responseCode);
-	return _responseCode;
+[[nodiscard]] long Scrafurl::get_response_code() const noexcept {
+	curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &_response_code);
+	return _response_code;
 }
 
-[[nodiscard]] const std::string& Scrafurl::getResponseBody() const noexcept {
-	return _responseBody;
+[[nodiscard]] const std::string& Scrafurl::get_response_body() const noexcept {
+	return _response_body;
 }
 
-std::size_t Scrafurl::writeData(char* const incomingBuffer, const std::size_t size, const std::size_t count, std::string* data) {
+std::size_t Scrafurl::write_data(char* const incomingBuffer, const std::size_t size, const std::size_t count, std::string* data) {
 	data->append(incomingBuffer, size * count);
 	return size * count;
 }
